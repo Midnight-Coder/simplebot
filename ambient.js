6c149c7b0832fc3bf19ff5b53ecfa1e5fa4adf0c
Sagar Jhobalia <jhobalia@yahoo-inc.com>
2014-12-13 14:38:17 -0800

    Blinks led for 1s on abnormal light/sound detection

--- ambient.js
+++ ambient.js
@@ -12,8 +12,8 @@ var ambientlib = require('ambient-attx4');
 
 var ambient = ambientlib.use(tessel.port['B']);
 //Onboard leds
-var led1 = tessel.led[0].output(0);
-var led2 = tessel.led[1].output(0);
+var ledYellow = tessel.led[0].output(0);
+var ledBlue = tessel.led[1].output(0);
 
 ambient.on('ready', function () {
  // Get points of light and sound data.
@@ -28,43 +28,47 @@ ambient.on('ready', function () {
 
   ambient.setLightTrigger(0.5);
 
-  // Set a light level trigger
-  // The trigger is a float between 0 and 1
+  // Set a light level trigger: 0-1
   ambient.on('light-trigger', function(data) {
     console.log("Our light trigger was hit:", data);
-    led2.toggle();
+    toggleLED(ledBlue, 100, 1000);
 
     // Clear the trigger so it stops firing
     ambient.clearLightTrigger();
     //After 1.5 seconds reset light trigger
     setTimeout(function () {
-
         ambient.setLightTrigger(0.5);
-
     },1500);
   });
 
-  // Set a sound level trigger
-  // The trigger is a float between 0 and 1
+  // Set a sound level trigger: 0-1
   ambient.setSoundTrigger(0.2);
 
   ambient.on('sound-trigger', function(data) {
     console.log("Something happened with sound: ", data);
-    led1.toggle();
-
-    // Clear it
+    toggleLED(ledYellow, 100, 1000);
     ambient.clearSoundTrigger();
 
     //After 1.5 seconds reset sound trigger
     setTimeout(function () {
-
         ambient.setSoundTrigger(0.2);
-
     },1500);
-
   });
 });
 
 ambient.on('error', function (err) {
   console.log(err)
 });
+
+function toggleLED(led, interval, duration){
+  var intervalID;
+  interval = interval || 100;
+  duration = duration || 2000;
+
+  intervalID = setInterval(function () {
+    led.toggle();
+  }, interval);
+  setTimeout(function() {
+    clearInterval(intervalID);
+  }, duration);
+}
