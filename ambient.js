9c97fb8ee2c2d85bdd927be40d0cfa94db65d522
Sagar Jhobalia <jhobalia@yahoo-inc.com>
2014-12-13 16:31:47 -0800

    Integrates servo with ambient.Work in progress: Monitor places to find the most quiet and amply lit place

--- ambient.js
+++ ambient.js
@@ -7,57 +7,98 @@ ambient light and sound levels and whenever a
 specified light or sound level trigger is met.
 *********************************************/
 
-var tessel = require('tessel');
-var ambientlib = require('ambient-attx4');
+var tessel = require('tessel'),
+  ambientlib = require('ambient-attx4'),
+  servolib = require('servo-pca9685');
 
-var ambient = ambientlib.use(tessel.port['B']);
+var servo = servolib.use(tessel.port['A']),
+  ambient = ambientlib.use(tessel.port['B']);
+
 //Onboard leds
-var ledYellow = tessel.led[0].output(0);
-var ledBlue = tessel.led[1].output(0);
+var ledYellow = tessel.led[0].output(0),
+    ledBlue = tessel.led[1].output(0),
+    servo1 = 1,
+    servo2 = 2;
+
+var soundLevel = {
+    pindrop: 0.01,
+    quiet: 0.01464844,
+    ambient: 0.01660156,
+    loud: 0.02832031
+};
+var lightLevel = {
+    ample: 0.07324219,
+    dim: 0.03320312,
+    flash: 0.513671875
+};
 
 ambient.on('ready', function () {
- // Get points of light and sound data.
-  setInterval( function () {
-    ambient.getLightLevel( function(err, ldata) {
-      if (err) throw err;
-      ambient.getSoundLevel( function(err, sdata) {
-        if (err) throw err;
-        console.log("Light level:", ldata.toFixed(8), " ", "Sound Level:", sdata.toFixed(8));
-    });
-  })}, 500); // The readings will happen every .5 seconds unless the trigger is hit
-
-  ambient.setLightTrigger(0.5);
-
-  // Set a light level trigger: 0-1
+  // React to bright flashes of light
+  ambient.setLightTrigger(lightLevel.flash);
   ambient.on('light-trigger', function(data) {
     console.log("Our light trigger was hit:", data);
     toggleLED(ledBlue, 100, 1000);
-
-    // Clear the trigger so it stops firing
+    // Clear and reset the trigger
     ambient.clearLightTrigger();
-    //After 1.5 seconds reset light trigger
     setTimeout(function () {
-        ambient.setLightTrigger(0.5);
+        ambient.setLightTrigger(lightLevel.flash);
     },1500);
   });
 
-  // Set a sound level trigger: 0-1
-  ambient.setSoundTrigger(0.2);
-
+  // React to loud sounds
+  ambient.setSoundTrigger(soundLevel.loud);
   ambient.on('sound-trigger', function(data) {
     console.log("Something happened with sound: ", data);
     toggleLED(ledYellow, 100, 1000);
-    ambient.clearSoundTrigger();
 
-    //After 1.5 seconds reset sound trigger
+    // Clear and reset the trigger
+    ambient.clearSoundTrigger();
     setTimeout(function () {
-        ambient.setSoundTrigger(0.2);
+        ambient.setSoundTrigger(soundLevel.loud);
     },1500);
   });
 });
 
 ambient.on('error', function (err) {
   console.log(err)
+});
+
+servo.on('ready', function () {
+  var forward = 0.8,
+    backward = 0.2,
+    stop = 0.0815;
+
+  servo.configure(servo1, 0, 1, function () {
+    var intervalID = setInterval(function () {
+      servo.move(servo1, forward);
+    }, 500);
+
+    setTimeout(function() {
+      clearInterval(intervalID);
+      readServoValue(servo1);
+      servo.move(servo1, stop);
+      readServoValue(servo1);
+    }, 5000);
+  });
+
+  servo.configure(servo2, 0, 1, function () {
+    var intervalID = setInterval(function () {
+      servo.move(servo2, backward);
+      readServoValue(servo2);
+    }, 500);
+
+    setTimeout(function() {
+      clearInterval(intervalID);
+      readServoValue(servo2);
+      servo.move(servo2, stop);
+      servo.move(servo1, stop);
+      readServoValue(servo2);
+    }, 5000);
+  });
+
+});
+servo.on('error', function(err){
+  console.log("Servo Err!!", err);
 });
 
 function toggleLED(led, interval, duration){
@@ -71,4 +112,13 @@ function toggleLED(led, interval, duration){
   setTimeout(function() {
     clearInterval(intervalID);
   }, duration);
+}
+
+function readServoValue(servoName){
+  servo.read( servoName, function(err, reading){
+        if(err)
+          console.log("error in reading : " + err);
+        else
+          console.log(servoName + "reading: " + reading);
+      });
 }
--- servo.js
+++ servo.js
@@ -14,28 +14,53 @@ var servolib = require('servo-pca9685');
 var servo = servolib.use(tessel.port['A']);
 
 var servo1 = 1; // We have a servo plugged in at position 1
-var servo2 = 2; // We have another servo plugged in at position 2
+// var servo2 = 2; // We have another servo plugged in at position 2
 servo.on('ready', function () {
-  var forward = 1,
-    backward = 0,
-    stop = 0.5;
+  var forward = 0.8,
+    backward = 0.2,
+    stop = 0.0815;
 
-  //  Set the minimum and maximum duty cycle for servo 1.
+  //  Set the minimum and maximum duty cycle for servo.
   //  If the servo doesn't move to its full extent or stalls out
   //  and gets hot, try tuning these values (0.05 and 0.12).
   //  Moving them towards each other = less movement range
   //  Moving them apart = more range, more likely to stall and burn out
+
   servo.configure(servo1, 0.05, 0.12, function () {
-    setInterval(function () {
+    var intervalID = setInterval(function () {
       console.log('Servo 1 Position (in range 0-1):', forward);
       servo.move(servo1, forward);
     }, 500); // Every 500 milliseconds
+
+    setTimeout(function() {
+      console.log('Servo 1 Position stop');
+      clearInterval(intervalID);
+
+      servo.read( servo1, function(err, reading){
+        if(err)
+          console.log("error in reading : " + err);
+        else
+          console.log("servo1 reading: " + reading);
+      });
+
+      servo.move(servo1, stop);
+
+      servo.read( servo1, function(err, reading){
+        if(err)
+          console.log("error in reading : " + err);
+        else
+          console.log("servo1 reading: " + reading);
+      });
+    }, 3000);
   });
 
-  servo.configure(servo2, 0.05, 0.12, function () {
-    setInterval(function () {
-      console.log('Servo 2 Position (in range 0-1):', backward);
-      servo.move(servo2, backward);
-    }, 500); // Every 500 milliseconds
-  });
+  // servo.configure(servo2, 0.05, 0.12, function () {
+  //   setInterval(function () {
+  //     console.log('Servo 2 Position (in range 0-1):', backward);
+  //     servo.move(servo2, backward);
+  //   }, 500); // Every 500 milliseconds
+  // });
+});
+servo.on('error', function(err){
+  console.log("Err!!", err);
 });
